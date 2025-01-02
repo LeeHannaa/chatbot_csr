eacec64234cbabdb3da435cfa5b595a718a3602d
LeeHannaa <103620720+LeeHannaa@users.noreply.github.com>
2025-01-02 10:29:40 +0900

    error: 채팅창 무한 스크롤 에러

--- src/components/ChatView/ChatRoomComponent.vue
+++ src/components/ChatView/ChatRoomComponent.vue
@@ -1,24 +1,7 @@
 <script setup lang="ts">
-import { onMounted, ref, watch } from 'vue'
 import { useConversationStore } from '../../stores/conversation'
 import InputChatComponent from '../ChatView/InputChatComponent.vue'
 
-const chatContainer = ref<HTMLElement | null>(null)
-onMounted(() => {
-  // 컴포넌트가 마운트되었을 때 스크롤을 맨 아래로 설정
-  if (chatContainer.value) {
-    chatContainer.value.scrollTop = chatContainer.value.scrollHeight
-  }
-})
-// 대화 내용이 업데이트될 때마다 스크롤을 아래로 이동
-watch(
-  () => conversationStore.formattedConversation,
-  () => {
-    if (chatContainer.value) {
-      chatContainer.value.scrollTop = chatContainer.value.scrollHeight
-    }
-  },
-)
 const conversationStore = useConversationStore()
 defineProps<{
   toggleFAQ: () => void
